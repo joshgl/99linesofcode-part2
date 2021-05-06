71d10a4deb1145b79d4307e11f1b49fd8261bec8
Josh Garlick <joshua.m.garlick@gmail.com>
2021-05-06 07:36:02 -0400

    for review

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
     let friends = ["Bobby", "Jacob", "Trish", "Mike", "Morgan"];
+    let body = document.body;
+    let buttonCreate = document.createElement("button");
+    let buttonText = document.createTextNode("Sing!");
 
-    friends.forEach(function (name){
+    window.addEventListener("DOMContentLoaded", function () {
+    buttonCreate.appendChild(buttonText);
+    body.appendChild(buttonCreate);
+    }
+    )
+
+    buttonCreate.addEventListener("click", function () {
+    friends.forEach(function (name, index){
+        let newFriendDiv = document.createElement("div");
+        let newFriendHeader = document.createElement("h3");
+        let newFriendParagraph = document.createElement("p");
+
+        newFriendDiv.setAttribute("class", "friend");
+        newFriendHeader.innerText = name;
+        newFriendDiv.appendChild(newFriendHeader);
+        newFriendHeader.appendChild(newFriendParagraph);
+        
         for (var j = 99; j > 0 ; j--){
             if (j > 1) {
-                console.log( j + " lines of code in the file, " + j + " lines of code; " + name + " strikes one out, clears it all out, " + (j - 1) +  " lines of code in the file");
+              newFriendParagraph.innerHTML += j + " lines of code in the file, " + j + " lines of code; " + name + " strikes one out, clears it all out, " + (j - 1) +  " lines of code in the file" + "<br />"; 
              } else {
-               console.log("1 line of code in the file, 1 line of code; " + name + " strikes one out, clears it all out, 0 lines of code in the file");
+              newFriendParagraph.innerHTML += "1 line of code in the file, 1 line of code; " + name + " strikes one out, clears it all out, 0 lines of code in the file" + "<br />";
      
              }
               
           }
 
-    });
+          body.appendChild(newFriendDiv);
+
+    })
+    }
+
+    );
